d92a12a6de50f9eafcced6f39b536d94dd3cca1e
richo <richo@psych0tik.net>
2012-05-24 01:15:56 +1000

    Sample config file

---
+++ devious-config.scm
@@ -0,0 +1,16 @@
+; This file is loaded at boot time by devious and processed as a scheme source
+; file.
+;
+; The valid values are shown below commented out, however they are asserted by
+; the code and so you should only change them if you need to to gaurantee
+; forward compatibility
+;
+;
+;(define devious-server  "irc.psych0tik.net")
+;(define devious-port    6697)
+;(define devious-ssl?    #t)
+;
+;(define devious-nick    "devious")
+;(define devious-name    "devious")
+;
+;(define devious-channels (list "#hackery"))
